393c69142ff9d3e839020b3731c6f3cc6779f539
Rabia Bhatti <rabia.iqbal.bhatti@gmail.com>
2019-08-20 19:02:51 +0500

    Add express session in graphql server

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,10 +1,14 @@
 "use strict";
 
+var _ms = _interopRequireDefault(require("ms"));
+
 var _path = _interopRequireDefault(require("path"));
 
-var _models = require("./models");
+var _expressSession = _interopRequireDefault(require("express-session"));
 
 var _graphqlYoga = require("graphql-yoga");
+
+var _models = require("./models");
 
 var _resolvers = _interopRequireDefault(require("./resolvers"));
 
@@ -14,10 +18,25 @@ _models.sequelize.sync({
   force: true
 }).catch(console.error);
 
+const context = req => ({
+  req: req.request
+});
+
 const server = new _graphqlYoga.GraphQLServer({
   typeDefs: _path.default.resolve(__dirname, '../schema.graphql'),
-  resolvers: _resolvers.default
+  resolvers: _resolvers.default,
+  context
 });
+server.express.use((0, _expressSession.default)({
+  name: 'qid',
+  secret: `some-random-secret-here`,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: (0, _ms.default)('1d')
+  }
+}));
 server.start({
   port: 9000
 }, () => console.log('Server is running on localhost:9000')).catch(err => console.error(err));
--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,32 @@
+import ms from 'ms'
 import path from 'path'
-import {sequelize} from './models'
+import session from 'express-session'
 import { GraphQLServer } from 'graphql-yoga'
+
+import {sequelize} from './models'
 import resolvers from './resolvers'
 
 sequelize.sync({ force: true }).catch(console.error)
-const server = new GraphQLServer({ typeDefs: path.resolve(__dirname, '../schema.graphql'), resolvers })
+
+const context = (req) => ({
+    req: req.request,
+})
+
+const server = new GraphQLServer({
+    typeDefs: path.resolve(__dirname, '../schema.graphql'),
+    resolvers,
+    context,
+})
+server.express.use(session({
+    name: 'qid',
+    secret: `some-random-secret-here`,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: ms('1d'),
+    },
+}))
+
 server.start({ port: 9000 }, () => console.log('Server is running on localhost:9000'))
     .catch(err => console.error(err))
